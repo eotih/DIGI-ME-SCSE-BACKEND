cb575b10d16bb5ed1634d93224182598f6d59c3c
enditidev <enditidev@gmail.com>
2021-10-05 15:53:02 +0700

    da fix getby the loai

---
+++ SCSE-BACKEND/Models/Posts1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCSE_BACKEND.Models
+{
+    public class Posts1
+    {
+        public int IDPost { get; set; }
+        public int IDCat { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public Nullable<System.DateTime> CreatedByDate { get; set; }
+        public string Author { get; set; }
+        public Nullable<bool> Status { get; set; }
+
+    }
+}
