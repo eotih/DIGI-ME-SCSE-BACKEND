ad5a23dc811cfe10943a6c8a1f4f8a6fc5da717f
Le Trong Hieu <eotihvn@gmail.com>
2021-10-10 14:27:31 +0700

    bản final v1.0

---
+++ SCSE-BACKEND/Models/FieldName1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCSE_BACKEND.Models
+{
+    public class FieldName1
+    {
+        public int ID { get; set; }
+        public string Field { get; set; }
+        public Nullable<int> IDField { get; set; }
+    }
+}
