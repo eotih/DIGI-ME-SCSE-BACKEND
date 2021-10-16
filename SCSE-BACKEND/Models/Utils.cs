8809a97947095e43115fb7af43528e6fc60fb783
enditidev <enditidev@gmail.com>
2021-10-16 17:46:04 +0700

    Cải cách lần thứ 1.1

---
+++ SCSE-BACKEND/Models/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SCSE_BACKEND.Models
+{
+    public class Utils
+    {
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+            }
+            return byte2String;
+        }
+        
+        public static string ReplaceSpecialChars(string str)
+        {
+            string[] chars = new string[] { " ", ",", ".", "/", "!", "@", "#", "$", "%", "^", "&", "*", "'", "\"", ";", "_", "(", ")", ":", "|", "[", "]" };
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (str.Contains(chars[i]))
+                {
+                    str = str.Replace(chars[i], "-");
+                }
+            }
+            return str;
+        }
+    }
+}
