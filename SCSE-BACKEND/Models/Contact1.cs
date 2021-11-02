33e9124ea797e6e3d640b006338d279dd950863e
enditidev <enditidev@gmail.com>
2021-11-02 14:30:43 +0700

    Done V1

---
+++ SCSE-BACKEND/Models/Contact1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCSE_BACKEND.Models
+{
+    public class Contact1
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Details { get; set; }
+        public System.DateTime CreatedByDate { get; set; }
+        public Nullable<System.DateTime> UpdatedByDate { get; set; }
+        public string Subtitle { get; set; }
+    }
+}
---
+++ SCSE-BACKEND/Models/Document1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCSE_BACKEND.Models
+{
+    public class Document1
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Details { get; set; }
+        public Nullable<System.DateTime> CreatedByDate { get; set; }
+        public Nullable<System.DateTime> UpdatedByDate { get; set; }
+    }
+}
